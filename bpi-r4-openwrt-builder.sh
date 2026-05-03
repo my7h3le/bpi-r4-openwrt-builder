6a10ad6505106aad700c2eb05a15162b7be8b604
Tash <mandate-word-dupe@duck.com>
2026-05-03 14:16:10 -0400

    use openwrt 25.12 instead of 24.10

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -4,11 +4,13 @@ set -euo pipefail
 rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
-git clone --branch openwrt-24.10 https://git.openwrt.org/openwrt/openwrt.git openwrt
-cd openwrt; git checkout 77d54cca84ffbb7754d592712ee96b0fcbff0d92; cd -;		#jsonfilter: fix memory leak in jsonfilter
+git clone --branch master https://git.openwrt.org/openwrt/openwrt.git openwrt
+cd openwrt; git checkout 8a18e84a887379db8954449a9fabe6df54084dea; cd -;
 
 git clone --branch master https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds
-cd mtk-openwrt-feeds; git checkout 1823bb66e40b3a5db7b0a8b26e7022c074fbe3f2; cd -;	#[MAC80211][kernel-6.6/kernel-6.12][wed][Add WED version3 support RROQM bypass wo]
+cd mtk-openwrt-feeds; git checkout 5ee86b89bc62dee10d8db1e0a48fdd140dd2ec3e; cd -;
+
+\cp -r my_files/w-feed_revision ./mtk-openwrt-feeds/autobuild/unified/feed_revision
 
 ### wireless-regdb modification - this remove all regdb wireless countries restrictions
 #rm -rf openwrt/package/firmware/wireless-regdb/patches/*.*
@@ -16,35 +18,39 @@ cd mtk-openwrt-feeds; git checkout 1823bb66e40b3a5db7b0a8b26e7022c074fbe3f2; cd 
 #\cp -r my_files/500-tx_power.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches
 #\cp -r my_files/regdb.Makefile openwrt/package/firmware/wireless-regdb/Makefile
 
-rm -rf mtk-openwrt-feeds/24.10/patches-feeds/108-strongswan-add-uci-support.patch
+#rm -rf mtk-openwrt-feeds/25.12/patches-feeds/strongswan-01-add-uci-support.patch
 
-\cp -r my_files/9999-sfp-10-additional-quirks.patch mtk-openwrt-feeds/24.10/files/target/linux/mediatek/patches-6.6
+\cp -r my_files/9999-sfp-10-additional-quirks.patch mtk-openwrt-feeds/25.12/files/target/linux/mediatek/patches-6.6
 
 ### tx_power patch - required for BE14 boards with defective eeprom flash
-\cp -r my_files/99999_tx_power_check.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches/
-\cp -r my_files/1007-wozi-arch-arm64-dts-mt7988a-add-thermal-zone.patch mtk-openwrt-feeds/24.10/patches-base/
+#\cp -r my_files/99999_tx_power_check.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/25.12/files/package/kernel/mt76/patches/
+#\cp -r my_files/1007-wozi-arch-arm64-dts-mt7988a-add-thermal-zone.patch mtk-openwrt-feeds/25.12/patches-base/
 
 cd openwrt
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 prepare
+#bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 prepare
+# Select one SKU to build
+## All-In-One MTK Reference Board (RFB) and BananaPi BPI-R4
+bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic prepare
 
-\cp -r ../my_files/sms-tool/ feeds/packages/utils/sms-tool
-\cp -r ../my_files/modemdata-main/ feeds/packages/utils/modemdata 
-\cp -r ../my_files/luci-app-modemdata-main/luci-app-modemdata/ feeds/luci/applications
-\cp -r ../my_files/luci-app-lite-watchdog/ feeds/luci/applications
-\cp -r ../my_files/luci-app-sms-tool-js-main/luci-app-sms-tool-js/ feeds/luci/applications
+# \cp -r ../my_files/sms-tool/ feeds/packages/utils/sms-tool
+# \cp -r ../my_files/modemdata-main/ feeds/packages/utils/modemdata 
+# \cp -r ../my_files/luci-app-modemdata-main/luci-app-modemdata/ feeds/luci/applications
+# \cp -r ../my_files/luci-app-lite-watchdog/ feeds/luci/applications
+# \cp -r ../my_files/luci-app-sms-tool-js-main/luci-app-sms-tool-js/ feeds/luci/applications
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-\cp -r ../my_files/qmi.sh package/network/utils/uqmi/files/lib/netifd/proto/
-chmod -R 755 package/network/utils/uqmi/files/lib/netifd/proto
-chmod -R 755 feeds/luci/applications/luci-app-modemdata/root
-chmod -R 755 feeds/luci/applications/luci-app-sms-tool-js/root
-chmod -R 755 feeds/packages/utils/modemdata/files/usr/share
+# \cp -r ../my_files/qmi.sh package/network/utils/uqmi/files/lib/netifd/proto/
+# chmod -R 755 package/network/utils/uqmi/files/lib/netifd/proto
+# chmod -R 755 feeds/luci/applications/luci-app-modemdata/root
+# chmod -R 755 feeds/luci/applications/luci-app-sms-tool-js/root
+# chmod -R 755 feeds/packages/utils/modemdata/files/usr/share
 
 #cd openwrt
 \cp -r ../my_files/my_final_defconfig .config
 make defconfig
 
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 build
+#bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 build
+bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic build
 
